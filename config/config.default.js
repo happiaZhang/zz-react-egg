9ea4f4f936985d4c0a3b7102ec45090f144e1809
张政 <zhangzheng44@wanda.cn>
2017-10-31 17:35:48 +0800

    eslint

--- config/config.default.js
+++ config/config.default.js
@@ -4,29 +4,29 @@
  * 基础配置
  */
 module.exports = appInfo => {
-    const config = {};
+  const config = {};
 
-    // app key
-    config.keys = 'wandacloud-bss-node';
+  // app key
+  config.keys = 'wandacloud-bss-node';
 
-    // view 模版
-    config.view = {
-        defaultViewEngine: 'nunjucks',
-        defaultExtension: '.html',
-    };
+  // view 模版
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    defaultExtension: '.html'
+  };
 
-    // 加载 errorHandler 中间件
-    // config.middleware = ['errorHandler', 'loginer'];
-    config.middleware = ['errorHandler'];
-    // 只对 /api 前缀的 url 路径生效
-    config.errorHandler = {
-        match: '/api',
-    };
+  // 加载 errorHandler 中间件
+  // config.middleware = ['errorHandler', 'loginer'];
+  config.middleware = ['errorHandler'];
+  // 只对 /api 前缀的 url 路径生效
+  config.errorHandler = {
+    match: '/api'
+  };
 
-    // 统一错误定义
-    config.errors = require('./errors/config');
-    // 正则表达式
-    config.regex = require('./regex/config');
+  // 统一错误定义
+  config.errors = require('./errors/config');
+  // 正则表达式
+  config.regex = require('./regex/config');
 
-    return config;
+  return config;
 };
