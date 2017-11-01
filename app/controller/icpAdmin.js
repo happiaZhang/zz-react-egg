03f6c32081ee024dea466725017eb2f8527b4eee
张政 <zhangzheng44@wanda.cn>
2017-11-01 18:36:59 +0800

    set content-type property on request headers

--- app/controller/icpAdmin.js
+++ app/controller/icpAdmin.js
@@ -4,34 +4,34 @@
  * ICP模块控制器
  */
 module.exports = app => {
-
-    return class IcpAdminController extends app.Controller {
-        constructor(ctx) {
-            super(ctx);
-            this.prefix = '/api/icp-admin';
-            this.host = this.config.icpAdminHost;
-        }
-
-        async action() {
-            const { ctx } = this;
-            const body = ctx.request.body;
-            const url = this.host + ctx.url.substr(this.prefix.length);
-            const payload = {
-                dataType: 'json',
-                method: ctx.method
-            };
-            if (body) payload.data = ctx.request.body;
-            console.log(payload.data);
-            // 获取数据
-            const res = await app.curl(url, payload);
-
-            console.log(res);
-            ctx.restfulResultCheck(res);
-
-            // 成功返回
-            ctx.helper.json(0, 'success', res.data);
-        }
-
-
+  return class IcpAdminController extends app.Controller {
+    constructor(ctx) {
+      super(ctx);
+      this.prefix = '/api/icp-admin';
+      this.host = this.config.icpAdminHost;
     }
+
+    async action() {
+      const {ctx} = this;
+      const body = ctx.request.body;
+      const url = this.host + ctx.url.substr(this.prefix.length);
+      const payload = {
+        dataType: 'json',
+        method: ctx.method,
+        headers: {
+          'content-type': 'application/json'
+        }
+      };
+      if (body) payload.data = ctx.request.body;
+      console.log(payload.data);
+      // 获取数据
+      const res = await app.curl(url, payload);
+
+      console.log(res);
+      ctx.restfulResultCheck(res);
+
+      // 成功返回
+      ctx.helper.json(0, 'success', res.data);
+    }
+  };
 };
