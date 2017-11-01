910436915a702b9eda88eb13cbe3a62b1db2fc8f
张政 <zhangzheng44@wanda.cn>
2017-11-01 11:56:28 +0800

    add unit test code

---
+++ test/app/controller/admin.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const {app, mock, assert} = require('egg-mock/bootstrap');
+const {loginAction} = require('../utils/apis');
+
+describe('test/app/controller/admin.test.js', () => {
+  beforeEach(async() => {
+  });
+});
