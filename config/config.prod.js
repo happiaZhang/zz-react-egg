82225a80eedd89921866bc77d9ee258345456d85
张政 <zhangzheng44@wanda.cn>
2017-11-16 15:55:02 +0800

    set egg production configuration

--- config/config.prod.js
+++ config/config.prod.js
@@ -8,6 +8,12 @@ module.exports = {
   adminHost: 'http://10.214.169.111:32730',
   icpAdminHost: 'http://10.214.169.111:32356',
 
+  // middleware
+  middleware: ['errorHandler', 'logger', 'isLogin', 'isPermission'],
+  errorHandler: {
+    match: '/api'
+  },
+
   // redis
   redis: {
     client: {
@@ -16,5 +22,16 @@ module.exports = {
       password: '',
       db: 0
     }
+  },
+
+  // log配置
+  logger: {
+    appLogName: `${appInfo.name}-web.log`,
+    coreLogName: `${appInfo.name}.log`,
+    agentLogName: `${appInfo.name}-agent.log`,
+    errorLogName: `${appInfo.name}-common-error.log`,
+    dir: `/var/log/${appInfo.name}`,
+    consoleLevel: 'DEBUG',
+    disableConsoleAfterReady: false
   }
 };
