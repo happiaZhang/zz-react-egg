92293e7cbaeb1b1d0f7694c08d48f32689f5b814
张政 <zhangzheng44@wanda.cn>
2017-11-23 15:51:06 +0800

    optimization of heart beat mechanism

--- app/controller/health.js
+++ app/controller/health.js
@@ -3,32 +3,29 @@
  * 健康机制
  */
 module.exports = app => {
-    return class HealthController extends app.Controller {
+  return class HealthController extends app.Controller {
+    /**
+     * 容器正常启动
+     */
+    async liveness() {
+      const {ctx} = this;
 
-        /**
-         * 容器正常启动
-         */
-        async liveness() {
-            const { ctx } = this;
-
-            ctx.status = 200;
-            ctx.body = 'success';
-        }
-
-        /**
-         * 容器所提供的服务正常
-         */
-        async readiness() {
-            const { ctx } = this;
-
-            //redis访问
-            await ctx.app.redis.set('readiness', 'success', 'EX', 30);
-            const data = await ctx.app.redis.get('readiness');
-
-            ctx.status = 200;
-            ctx.body = data;
-
-        }
-
+      ctx.status = 200;
+      ctx.body = 'success';
     }
+
+    /**
+     * 容器所提供的服务正常
+     */
+    async readiness() {
+      const {ctx} = this;
+
+      // redis访问
+      // await ctx.app.redis.set('readiness', 'success', 'EX', 30);
+      const data = await ctx.app.redis.get('readiness');
+
+      ctx.status = 200;
+      ctx.body = data;
+    }
+  };
 };
